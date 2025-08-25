807121c0904057343eeb39467d7bcc60d61cc35f
danny bengal <febrero.daniel@gmail.com>
2025-08-25 16:29:08 +0200

    prep for stage

--- scripts/deploy-backend.sh
+++ scripts/deploy-backend.sh
@@ -6,7 +6,7 @@ if [ -f "./backend/.docker-image-uri" ]; then
     cd backend && npm run build:docker
     IMAGE_URI=$(cat ./.docker-image-uri | tr -d '\n')
     echo "Using ImageUri: $IMAGE_URI"
-    cd .. && sam build && sam deploy --config-env prod --parameter-overrides ImageUri="$IMAGE_URI"
+    cd .. && sam build && sam deploy --config-env "${ENVIRONMENT}" --parameter-overrides ImageUri="$IMAGE_URI"
 else
     echo "Error: .docker-image-uri file not found"
     exit 1
