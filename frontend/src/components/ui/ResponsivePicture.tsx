8a679882c35030e11534eb1a64d412427daec330
danny bengal <febrero.daniel@gmail.com>
2025-08-29 02:28:44 +0200

    add logger

--- frontend/src/components/ui/ResponsivePicture.tsx
+++ frontend/src/components/ui/ResponsivePicture.tsx
@@ -192,6 +192,7 @@ function getOptimalDefaultImageSrc(
   containerWidth: number,
   containerHeight: number
 ): string {
+  console.log({ thumbnailUrls, fallbackUrl, containerWidth, containerHeight });
   if (!thumbnailUrls) return fallbackUrl;
 
   const optimalSize = selectOptimalThumbnailSize(
