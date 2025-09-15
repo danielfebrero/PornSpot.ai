c6a32e5529fd0400004b67922229c8e8c28d6130
danny bengal <febrero.daniel@gmail.com>
2025-09-15 16:51:46 +0200

    less image per row

--- frontend/src/components/GenerateClient.tsx
+++ frontend/src/components/GenerateClient.tsx
@@ -1343,8 +1343,8 @@ export function GenerateClient() {
                       filteredGeneratedImages.length === 1
                         ? "grid-cols-1"
                         : deviceType === "mobile"
-                        ? "grid-cols-2"
-                        : "grid-cols-3"
+                        ? "grid-cols-1"
+                        : "grid-cols-2"
                     )}
                   >
                     {filteredGeneratedImages.map((image, index) => (
